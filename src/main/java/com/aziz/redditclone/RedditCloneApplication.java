0a3d0de38c6c7c9a469d88bfbd23b8e3f6ec6aa0
azizdev <30.azizbek@gmail.com>
2022-06-26 11:59:30 +0500

    Created entities and refractored using Project Lombok

--- src/main/java/com/aziz/redditclone/RedditCloneApplication.java
+++ src/main/java/com/aziz/redditclone/RedditCloneApplication.java
@@ -1,10 +1,14 @@
 package com.aziz.redditclone;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RedditCloneApplication {
+
+    private static final Logger log = LoggerFactory.getLogger(SpringApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(RedditCloneApplication.class, args);
