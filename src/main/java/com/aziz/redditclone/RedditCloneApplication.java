aae1256cbdc85f256ea49ddc53c1b3849f0ee73b
azizdev <30.azizbek@gmail.com>
2022-06-28 23:36:25 +0500

    Adding Views, images, css, bootstrap, BeanUtil(for PrettyTime)  and endpoints in LinkController

--- src/main/java/com/aziz/redditclone/RedditCloneApplication.java
+++ src/main/java/com/aziz/redditclone/RedditCloneApplication.java
@@ -1,12 +1,8 @@
 package com.aziz.redditclone;
 
-import com.aziz.redditclone.domain.Comment;
-import com.aziz.redditclone.domain.Link;
-import com.aziz.redditclone.repository.CommentRepository;
-import com.aziz.redditclone.repository.LinkRepository;
+import org.ocpsoft.prettytime.PrettyTime;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -22,20 +18,9 @@ public class RedditCloneApplication {
         SpringApplication.run(RedditCloneApplication.class, args);
     }
 
-//    @Bean
-    CommandLineRunner runner(LinkRepository linkRepository, CommentRepository commentRepository) {
-        return args -> {
-            Link link = new Link("Getting started with Spring boot2", "https://google.com");
-            linkRepository.save(link);
-
-            Comment comment = new Comment("This Spring boot 2 link is awesome!", link);
-            commentRepository.save(comment);
-            link.addComment(comment);
-
-            Comment comment2 = new Comment("Yeah this course good", link);
-            commentRepository.save(comment2);
-            link.addComment(comment2);
-        };
+    @Bean
+    PrettyTime prettyTime() {
+        return new PrettyTime();
     }
 
 }
