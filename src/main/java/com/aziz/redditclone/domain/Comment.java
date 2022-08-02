c42522278ea056ad2197e8e4d63c57505fa14182
azizdev <30.azizbek@gmail.com>
2022-08-02 23:10:57 +0500

    Added auditable things for Comment, comments for links, view of single comment

--- src/main/java/com/aziz/redditclone/domain/Comment.java
+++ src/main/java/com/aziz/redditclone/domain/Comment.java
@@ -1,11 +1,20 @@
 package com.aziz.redditclone.domain;
 
-import lombok.*;
+import com.aziz.redditclone.service.BeanUtil;
+import lombok.RequiredArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import org.ocpsoft.prettytime.PrettyTime;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
 
 
 @Entity
@@ -18,6 +27,7 @@ public class Comment extends Auditable {
     @Id
     @GeneratedValue
     private Long id;
+
     @NonNull
     private String body;
 
@@ -25,5 +35,14 @@ public class Comment extends Auditable {
     @ManyToOne
     @NonNull
     private Link link;
+
+    public String getPrettyTime() {
+        PrettyTime pt = BeanUtil.getBean(PrettyTime.class);
+        return pt.format(convertToDateViaInstant(getCreationDate()));
+    }
+
+    private Date convertToDateViaInstant(LocalDateTime dateToConvert) {
+        return java.util.Date.from(dateToConvert.atZone(ZoneId.systemDefault()).toInstant());
+    }
 
 }
