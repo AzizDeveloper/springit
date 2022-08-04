c7917b609ea222787a4aaa5d6f691cbd1ed49f6e
azizdev <30.azizbek@gmail.com>
2022-08-04 16:31:29 +0500

    Created UserService class and added UserService to AuthController

---
+++ src/main/java/com/aziz/redditclone/service/UserService.java
@@ -0,0 +1,26 @@
+package com.aziz.redditclone.service;
+
+import com.aziz.redditclone.domain.User;
+import com.aziz.redditclone.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private final Logger logger = LoggerFactory.getLogger(UserService.class);
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User register(User user) {
+        return user;
+    }
+
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+}
